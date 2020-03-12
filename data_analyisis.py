e1eb2db2fab9893ca4eed0a450acbd577e8f2748
MatCast <gozyssj4@gmail.com>
2020-03-12 09:10:45 +0100

    added function fto plot model

--- data_analyisis.py
+++ data_analyisis.py
@@ -12,17 +12,14 @@ data_url = ('https://raw.githubusercontent.com/CSSEGISandData/'
 df = pd.read_csv(data_url)
 per_day = df.groupby(['Country/Region']).sum()
 per_day['tot'] = per_day.drop(['Lat', 'Long'], axis=1).sum(axis=1)
-large = (per_day.loc[per_day['tot'].nlargest(11).index]
-         .drop(['Lat', 'Long', 'tot'], axis=1).drop(['Others']).T)
+large = (per_day.loc[per_day['tot'].index]
+         .drop(['Lat', 'Long', 'tot'],
+         axis=1, errors='ignore').drop(['Others'], errors='ignore').T)
 
 per_day['tot'].nlargest(11).index
-
 plt.figure(figsize=(8, 6))
 for land in large.columns:
     if not land == 'Mainland China':
-        X = sm.add_constant(large['Italy'][large['Italy'] > 50].values)
-        Y = np.array(range(len(X)))
-        model = sm.OLS(Y, X).fit()
         cases = large[land][large[land] > 50].values
         plt.plot(cases, label=land)
 plt.legend()
@@ -30,11 +27,26 @@ plt.ylabel('Number of Cases')
 plt.xlabel('Days since cases > 50')
 plt.yscale('log')
 plt.savefig('cases_per_land.png')
+plt.show()
 
-Y = np.log(large['Italy'][large['Italy'] > 50].values)
-X = sm.add_constant(np.array(range(len(X))))
-model = sm.OLS(Y, X).fit()
-print(model.summary())
-large['Italy'][large['Italy'] > 50].values[-2]/large['Italy'][large['Italy'] > 50].values[-3]
-# A=A0*exp(kt); k = log(A)/log(A0)
-plt.plot(51*np.exp(model.params[1]*np.arange(1, 30)))
+def predict_nation(nation, dasy_to_model=15, min_cases=50):
+    Y = np.log(large[nation][large[nation] > 50].values)
+    X = sm.add_constant(np.array(range(len(Y))))
+    model = sm.OLS(Y, X).fit()
+    print(model.summary())
+    italy = large[nation][large[nation] > min_cases].values
+    max_days = italy.shape[0] + dasy_to_model
+    modelled = (large[nation][large[nation] > min_cases]
+    .iloc[0]*np.exp(model.params[1]*np.arange(1, max_days)))
+    plt.title(nation)
+    plt.plot(modelled, label='Modello', linewidth=3.0)
+    plt.plot(italy, label='Dati Reali', linewidth=3.0)
+    plt.xticks(range(modelled.shape[0]))
+    plt.xlabel('Giorni Trascorsi')
+    plt.ylabel('Numero di contagiati')
+    plt.gca().yaxis.grid(True)
+    plt.legend()
+    plt.savefig('model_vs_relaity_{}.png'.format(nation))
+    return model
+
+model = predict_nation('Austria', 8, 5)
